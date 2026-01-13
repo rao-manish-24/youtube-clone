5bd6f8c0e0df59ff67e7d55f16ababb653515db7
Manish Rao <144836697+rao-manish-24@users.noreply.github.com>
2026-01-13 16:25:36 -0600

    Update imagekit_client.py

--- youtube/videos/imagekit_client.py
+++ youtube/videos/imagekit_client.py
@@ -2,12 +2,8 @@ import os
 from imagekitio import ImageKit
 
 
-def get_imagekit_client():
-    return ImageKit()
-        #public_key=os.environ.get("IMAGEKIT_PUBLIC_KEY"),
-        #private_key=os.environ.get("IMAGEKIT_PRIVATE_KEY"),
-        #url_endpoint=os.environ.get("IMAGEKIT_URL_ENDPOINT", "https://ik.imagekit.io/default")
-    
+# SDK 5.0 reads IMAGEKIT_PRIVATE_KEY from environment automatically
+imagekit = ImageKit()
 
 
 def _get_watermark_transformation(username: str):
@@ -34,32 +30,25 @@ def get_streaming_url(base_url: str) -> str:
     return f"{base_url}/ik-master.m3u8?tr=sr-240_360_480_720_1080"
 
 
-def get_thumbnail_url(
-        base_url: str, username: str = None
-) -> str:
+def get_thumbnail_url(base_url: str, username: str = None) -> str:
     transformations = "".join(_get_watermark_transformation(username))
-
     return f"{base_url}/ik-thumbnail.jpg?tr={transformations}"
 
 
-def add_image_watermark(
-        base_url: str, username: str = None
-) -> str:
+def add_image_watermark(base_url: str, username: str = None) -> str:
     transformations = "".join(_get_watermark_transformation(username))
-
     return f"{base_url}?tr={transformations}"
 
 
 def upload_video(file_data: bytes, file_name: str, folder: str = "videos") -> dict:
-    client = get_imagekit_client()
-    
+    # SDK 5.0 uses imagekit.files.upload()
     response = imagekit.files.upload(
         file=file_data,
         file_name=file_name,
         folder=folder,
         use_unique_file_name=True,
     )
-    
+
     return {
         "file_id": response.file_id,
         "url": response.url
@@ -68,24 +57,20 @@ def upload_video(file_data: bytes, file_name: str, folder: str = "videos") -> di
 
 def upload_thumbnail(file_data: str, file_name: str, folder: str = "thumbnails") -> dict:
     import base64
-    
+
     if file_data.startswith("data:"):
         base64_data = file_data.split(",", 1)[1]
         image_bytes = base64.b64decode(base64_data)
     else:
         image_bytes = base64.b64decode(file_data)
 
-    client = get_imagekit_client()
-    
-    response = client.upload_file(
+    response = imagekit.files.upload(
         file=image_bytes,
         file_name=file_name,
-        options={
-            "folder": folder,
-            "use_unique_file_name": True,
-        }
+        folder=folder,
+        use_unique_file_name=True,
     )
-    
+
     return {
         "file_id": response.file_id,
         "url": response.url
@@ -93,6 +78,5 @@ def upload_thumbnail(file_data: str, file_name: str, folder: str = "thumbnails")
 
 
 def delete_video(file_id: str) -> bool:
-    client = get_imagekit_client()
     imagekit.files.delete(file_id=file_id)
     return True
